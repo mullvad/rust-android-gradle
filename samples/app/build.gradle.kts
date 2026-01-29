49ce295a4f581866a7f8b9a8f4ad8b079a49aaae
David Göransson <david.goransson@mullvad.net>
2026-01-29 11:40:58 +0100

    ndk directory wip

--- samples/app/build.gradle.kts
+++ samples/app/build.gradle.kts
@@ -1,58 +1,56 @@
 plugins {
-    id("com.android.application") version("8.12.3")
-    id("net.mullvad.rust-android")
+  id("com.android.application") version ("9.0.0")
+  id("net.mullvad.rust-android")
 }
 
 android {
-    namespace = "net.mullvad.androidrust"
-    compileSdk = 35
-    ndkVersion = "27.3.13750724"
+  namespace = "net.mullvad.androidrust"
+  compileSdk = 35
+  ndkVersion = "27.3.13750724"
 
-    defaultConfig {
-        applicationId = "net.mullvad.androidrust"
-        minSdk = 21
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-        vectorDrawables.useSupportLibrary = true
+  defaultConfig {
+    applicationId = "net.mullvad.androidrust"
+    minSdk = 21
+    targetSdk = 35
+    versionCode = 1
+    versionName = "1.0"
+    vectorDrawables.useSupportLibrary = true
 
-        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+//      proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
     }
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
-    }
+    ndkPath =
 }
 
 
 cargo {
-    module = "../rust"
-    targets = listOf("x86_64", "arm64")
-    libname = "rust"
+  module = "../rust"
+  targets = listOf("x86_64", "arm64")
+  libname = "rust"
 
-    features {
-        defaultAnd("foo", "bar")
-        noDefaultBut("foo", "bar")
-        all()
-    }
+  features {
+    defaultAnd("foo", "bar")
+    noDefaultBut("foo", "bar")
+    all()
+  }
 }
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
+java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
 dependencies {
-    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2") {
-        exclude("com.android.support:support-annotations")
-    }
-    implementation("com.android.support:appcompat-v7:28.0.0")
-    implementation("com.android.support.constraint:constraint-layout:2.0.4")
-    testImplementation("junit:junit:4.13.2")
+  androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2") {
+    exclude("com.android.support:support-annotations")
+  }
+  implementation("com.android.support:appcompat-v7:28.0.0")
+  implementation("com.android.support.constraint:constraint-layout:2.0.4")
+  testImplementation("junit:junit:4.13.2")
 }
 
 val rustJniLibsDir = layout.buildDirectory.dir("rustJniLibs/android").get()
