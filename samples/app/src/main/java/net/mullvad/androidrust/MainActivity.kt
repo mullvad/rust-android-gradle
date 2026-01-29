78300a2469df1c17be8ff946016ed6c06e1b56a6
David Göransson <david.goransson@mullvad.net>
2026-01-29 14:18:31 +0100

    clean up sample

--- samples/app/src/main/java/net/mullvad/androidrust/MainActivity.kt
+++ samples/app/src/main/java/net/mullvad/androidrust/MainActivity.kt
@@ -6,64 +6,54 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.MutableStateFlow
-import net.mullvad.androidrust.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity(), JNICallback {
-  val state = MutableStateFlow<String?>(null)
+    val state = MutableStateFlow<String?>(null)
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
-    setContent {
-      MyApplicationTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          LaunchedEffect(Unit) { invokeCallbackViaJNI(this@MainActivity) }
-          val stringValue = state.collectAsStateWithLifecycle()
-          Greeting(
-              name = stringValue.value.toString(),
-              modifier = Modifier.padding(innerPadding),
-          )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LaunchedEffect(Unit) { invokeCallbackViaJNI(this@MainActivity) }
+                    val stringValue = state.collectAsStateWithLifecycle()
+                    Greeting(
+                        name = stringValue.value.toString(),
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
+            }
         }
-      }
     }
-  }
 
-  // Used to load the 'rust' library on application startup.
-  companion object {
-    init {
-      System.loadLibrary("rust")
+    // Used to load the 'rust' library on application startup.
+    companion object {
+        init {
+            System.loadLibrary("rust")
+        }
     }
-  }
 
-  /**
-   * A native method that is implemented by the 'rust' native library, which is packaged with this
-   * application.
-   */
-  external fun invokeCallbackViaJNI(callback: JNICallback?)
+    /**
+     * A native method that is implemented by the 'rust' native library, which is packaged with this
+     * application.
+     */
+    external fun invokeCallbackViaJNI(callback: JNICallback?)
 
-  override fun callback(string: String?) {
-    state.value = string
-  }
+    override fun callback(string: String?) {
+        state.value = string
+    }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-      text = "Hello $name!",
-      modifier = modifier,
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  MyApplicationTheme { Greeting("Android") }
+    Text(text = "$name!", modifier = modifier)
 }
