1dbeb5d117f4130ccc909b2cb94cdacaa7a93ebf
David Göransson <david.goransson@mullvad.net>
2026-01-30 16:22:45 +0100

    Test

--- samples/unittest/build.gradle.kts
+++ samples/unittest/build.gradle.kts
@@ -36,7 +36,7 @@ androidComponents {
 cargo {
     module = "../rust"
     // For MacBook we need to change the target
-    targets = listOf("darwin-aarch64", "aarch64-apple-darwin", "x86_64", "linux-x86-64")
+    targets = listOf("darwin-aarch64", "darwin-x86-64", "x86_64", "linux-x86-64")
     libname = "rust"
 }
 
